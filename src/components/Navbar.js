2a42c1ec1072046891eea85ba2b789b52e53c2cc
Bwalya Nonde <bwalyanonde8@gmail.com>
2022-07-22 13:50:29 -0400

    Add Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => (
+  <>
+    <div className="navbar">
+      <h1 className="nav-heading">Bookstore</h1>
+      <div className="nav-links">
+        <Link to="/">Books</Link>
+        <Link to="/categories">Categories</Link>
+      </div>
+    </div>
+  </>
+);
+
+export default Navbar;
